ebc986297808d4f99f72aabfef7e1dc0a208c15e
Anjana Ishara <anjanaishara08@gmail.com>
2021-08-28 12:33:07 +0530

    changed the landing path to splash

---
+++ src/Splash.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import {View} from 'react-native';
+export default class Splash extends Component{
+    constructor(props){
+        super();
+    }
+    render(){
+
+        return(
+            <View style={{flex:1, backgroundColor:'#ff00ff'}}>
+
+
+            </View>
+        )
+    }
+}
